ecefb75988dbe9672f03ec23d1e76c2d05f38cfb
Damian Aruj <aruj.damian@gmail.com>
2017-05-17 23:08:14 -0300

    wrapped the layout with the i18n module

--- src/generators/app/templates/src/shared/layout/index.jsx
+++ src/generators/app/templates/src/shared/layout/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
 import Routes from 'shared/routes';
 import Header from './Header';
+import Footer from './Footer';
 import '../assets/css/main.scss';
 
 const Layout = (props) => (
-  <div id="root">
-    <div className="wrap">
-      <Header />
-      <Routes />
+  <I18nextProvider i18n={props.i18n}>
+    <div id="root">
+      <div className="wrap">
+        <Header />
+        <Routes />
+        <Footer />
+      </div>
     </div>
-  </div>
+  </I18nextProvider>
 );
 
 Layout.propTypes = {};
