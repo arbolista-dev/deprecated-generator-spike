46868dee794b1c01e8ed4e1dec8b4e5a23509dd6
Eric Hulburd <eric@arbol.org>
2017-05-28 01:46:02 -0500

    fix build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 rm -r dist || mkdir dist
-npm run babel -- src/generators -d dist/generators --ignore test.js,templates/
-cp -r src/generators/app/templates dist/generators/app/templates
+npm run babel -- src -d dist --ignore test.js,templates/
+cp -r src/app/templates dist/app/templates
+cp -r src/actions/templates dist/actions/templates
 cp package.json dist
+cp README.md dist
