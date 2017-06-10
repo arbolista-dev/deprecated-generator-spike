199f2796451b2daf09e5b1d0f0dde62b14a2739a
Eric Hulburd <eric@arbol.org>
2017-06-10 13:16:02 -0500

    fix example app script

--- scripts/createExampleApp.sh
+++ scripts/createExampleApp.sh
@@ -1,12 +1,11 @@
-cd "${0%/*}" && cd ../dist
+cd "${0%/*}" && cd ..
 yarn global add yo
-yarn global remove .
-yarn global add .
-cd ..
+yarn global remove file:$PWD/dist
+yarn global add file:$PWD/dist
 rm -rf tmp/ && mkdir tmp
-yo spike -d tmp
-cd tmp
-yarn
+#yo spike -d tmp
+#cd tmp
+#yarn
 # npm run test
-npm run build
-cd ..
+#npm run build
+#cd ..
