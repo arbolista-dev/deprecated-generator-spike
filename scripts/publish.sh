9f40c23034e0a5e46320e44aa394316c205490eb
Eric Hulburd <eric@arbol.org>
2017-06-19 00:42:51 -0500

    changelog script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,7 +9,7 @@ git checkout develop &&\
   npm version "$1" &&\
   git add package.json &&\
   npm run changelog &&\
-  git add CHANGELOG.md &&\
+  git add CHANGELOG.md -- -n "$1" &&\
   sh scripts/build.sh &&\
   git commit -m "publish at $1" &\
   git push origin develop &&\
