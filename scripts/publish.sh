663e1276b42dbcf93c37220e35679ec2802108a0
Eric Hulburd <eric@arbol.org>
2017-06-19 01:10:19 -0500

    pass version to github changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,15 @@
 cd "${0%/*}" && cd ..
 if [ -z "$1" ]
   then
-    echo "No must supply a new package version"
+    echo "Must supply a new package version"
     exit 1
 fi
 git checkout develop &&\
   git pull origin develop &&\
   npm version "$1" &&\
   git add package.json &&\
-  npm run changelog &&\
-  git add CHANGELOG.md -- -n "$1" &&\
+  npm run changelog -- -n "$1" &&\
+  git add CHANGELOG.md &&\
   sh scripts/build.sh &&\
   git commit -m "publish at $1" &\
   git push origin develop &&\
